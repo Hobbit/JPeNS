034240013873358799c13cdb8e0704467754f75e
Babbitt <babbitt.j@gmail.com>
2012-04-11 20:00:19 -0700

    Updated the README with instructions on how to use. Updated the link to the default file for JPeNS

--- src/master/MainApp.java
+++ src/master/MainApp.java
@@ -7,8 +7,8 @@ public class MainApp
     public static void main(String[] args) {
         Petrinet pn = new Petrinet("My Net");
 
-		//data.xml is the default petrinet file to load on start
-        pn.setFilepath("src/large.xml");
+		//larger.xml is the default petrinet file to load on start
+        pn.setFilepath("src/larger.xml");
 		// At this point our importer should be modifying the existing pn Petrinet object
 		
         Importer.Import(pn.getFilepath(), pn);	
